901aa449ede18b1e6b85c2405b1718ae8917491b
El profe <al.sepulvedam@profesor.duoc.cl>
2024-08-21 12:56:46 -0400

    Rutas listas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { AyudaComponent } from './pages/ayuda/ayuda.component';
+import { NopageComponent } from './pages/nopage/nopage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'contacto',
+    component:ContactoComponent
+  },
+  {
+    path:'ayuda',
+    component:AyudaComponent
+  },
+  {
+    path:'**',
+    component:NopageComponent
+
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
